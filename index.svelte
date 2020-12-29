c9742d4892f46493cf1a8bf43b5d50bafd5a6bf8
rebotak <rebotak@gmail.com>
2020-12-29 13:42:41 +0700

    update: input handler mtehod

--- index.svelte
+++ index.svelte
@@ -4,7 +4,7 @@
   const KEYBOARD = {
     BACKSPACE: 8,
     DELETE: 46,
-    ANDROID_BACKSPACE: 229
+    ANDROID_BACKSPACE: 229,
   };
 
   let inputs = [0];
@@ -20,58 +20,68 @@
     document.getElementById("pin0").focus();
   });
 
-  const calcPin = pins => {
+  const calcPin = (pins) => {
     if (Object.values(pins).length) {
       return Object.values(pins).join("");
     } else return "";
   };
 
-  const changeHandler = function(e, i) {
-    let nextPin = e.target.nextElementSibling;
-    let prevPin = e.target.previousElementSibling;
-    let regx = new RegExp(/^\d+$/);
-    if (
-      (e.keyCode == KEYBOARD.BACKSPACE ||
-        e.keyCode == KEYBOARD.DELETE ||
-        e.keyCode == KEYBOARD.ANDROID_BACKSPACE) &&
-      !prevPin
-    ) {
-      pins[i] = "";
-    } else if (
-      (e.keyCode == KEYBOARD.BACKSPACE ||
-        e.keyCode == KEYBOARD.DELETE ||
-        e.keyCode == KEYBOARD.ANDROID_BACKSPACE) &&
-      prevPin
-    ) {
-      pins[i] = "";
-      prevPin.focus();
-    } else if (nextPin) {
-      if (regx.test(e.key)) {
-        pins[i] = e.key;
-      } else {
-        return;
-      }
-      setTimeout(() => {
-        nextPin.focus();
-      }, 0);
-    } else {
-      if (regx.test(e.key)) {
-        pins[i] = e.key;
-      } else {
-        return;
-      }
-    }
+  const isKeyDelete = (key) => {
+    let result =
+      key === KEYBOARD.BACKSPACE ||
+      key === KEYBOARD.DELETE ||
+      key === KEYBOARD.ANDROID_BACKSPACE;
+    return result;
+  };
 
+  const deleteCurrentPin = (i) => {
+    pins[i] = "";
     pin = calcPin(pins);
   };
-  const createArray = size => {
+
+  const changeHandler = function (e, i) {
+    const current = document.activeElement;
+    const items = [...document.getElementsByClassName("pin-item")];
+    const currentIndex = items.indexOf(current);
+    let newIndex;
+
+    let regx = new RegExp(/^\d+$/);
+    // backspace pressed on first digit
+    if (isKeyDelete(e.keyCode) && currentIndex === 0) {
+      // remove value of fist digit, end.
+      deleteCurrentPin(i);
+      return;
+    }
+    // pressing backspace not on the first digit
+    else if (isKeyDelete(e.keyCode) && currentIndex !== 0) {
+      // remove value of current digit & move to previous digit
+      deleteCurrentPin(i);
+      newIndex = (currentIndex + items.length - 1) % items.length;
+    } else {
+      // if not backspace pressed, set indext to next digit
+      newIndex = (currentIndex + 1) % items.length;
+    }
+    // test value. if number, change value of current digit
+    if (regx.test(e.key)) {
+      pins[i] = e.key;
+      pin = calcPin(pins);
+
+      current.blur();
+    }
+    // if not last digit, move cursor
+    if (currentIndex !== items.length - 1 || isKeyDelete(e.keyCode)) {
+      items[newIndex].focus();
+    }
+    return;
+  };
+  const createArray = (size) => {
     return new Array(size);
   };
-  const createValueSlot = arr => {
+  const createValueSlot = (arr) => {
     return arr.reduce((obj, item) => {
       return {
         ...obj,
-        [item]: ""
+        [item]: "",
       };
     }, {});
   };
@@ -110,11 +120,12 @@
       <input
         bind:value={pins[i]}
         maxLength="1"
+        class="pin-item"
         id={`pin${i}`}
         type="tel"
         pattern="\d{1}"
         maxlength="1"
-        on:keydown|preventDefault={event => changeHandler(event, i)}
+        on:keydown|preventDefault={(event) => changeHandler(event, i)}
         placeholder="" />
     {/each}
   {/if}
